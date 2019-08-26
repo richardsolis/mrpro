72a18a24c0e3a3e70297b0f229aa206aadaaa3dc
RICHARD SOLIS <RSL@MacBook-Pro-de-RICHARD.local>
2019-08-26 06:46:46 -0500

    anado cambio

--- src/app/platform/components/send-budget/send-budget.component.ts
+++ src/app/platform/components/send-budget/send-budget.component.ts
@@ -52,6 +52,7 @@ export class SendBudgetComponent implements OnInit {
   //Día
   d = this.ObjectServi.date_service.getDate();
   selectedImage;
+
   file: File;
   imgaa: FileList;
   constructor(private router: Router, private session: SessionService, private districtS: DistrictService, private userS: UserService, private generalS: GeneralService, private spinner: NgxSpinnerService) {}
@@ -94,18 +95,18 @@ export class SendBudgetComponent implements OnInit {
     this.sbutton = true;
     this.messageT = true;
     this.services = [];
-    var xd = [];
+    var dataSend = [];
     // this.ObjectServi.date_service = this.d + "-" + this.m + "-" + this.y + " " + this.hour;
     for (let i = 0; i < this.LocalProvider.length; i++) {
       this.ObjectServi.user_provider_id = this.LocalProvider[i].id;
-      xd.push(this.ObjectServi);
+      dataSend.push(this.ObjectServi);
     }
 
-    console.log(JSON.stringify(xd), "hoda");
+    console.log(JSON.stringify(dataSend), "hoda");
     console.log(this.ObjectServi, "asdadsdsa", this.d + "/" + this.m + "/" + this.y + this.hour, this.hour, this.LocalProvider);
     // let formData2: FormData = new FormData();
     // formData2.append("image_file", this.file);
-    this.userS.sendBudget({ services: JSON.stringify(xd) }).subscribe(
+    this.userS.sendBudget({ services: JSON.stringify(dataSend) }).subscribe(
       response => {
         this.message = "Se agendó un servicio";
         modal.open();
@@ -115,34 +116,6 @@ export class SendBudgetComponent implements OnInit {
         // this.generalS.relogin(this.sendBudget, error, this.spinner);
       }
     );
-
-    // this.budget.date_service = this.budget.date + " " + this.budget.hour;
-    // if (!this.budget.contact_name || !this.budget.phone_name || !this.budget.address || !this.budget.description || !this.budget.date_service) {
-    //   this.message = "Campos incompletos";
-    //   this.messageT = false;
-    //   this.sbutton = false;
-    //   modal.open();
-    //   return;
-    // }
-
-    // for (var i = this.providers.length - 1; i >= 0; i--) {
-    //   this.budget.provider_name = this.providers[i].user.name + " " + this.providers[i].user.lastname;
-    //   this.budget.provider = this.providers[i].id;
-    //   this.services.push(JSON.parse(JSON.stringify(this.budget)));
-    // }
-
-    // console.log(this.budget.date_service);
-    // this.spinner.show();
-    // this.userS.sendBudget({ services: this.services }).subscribe(
-    //   response => {
-    //     this.message = "Se agendó un servicio";
-    //     modal.open();
-    //     this.spinner.hide();
-    //   },
-    //   error => {
-    //     this.generalS.relogin(this.sendBudget, error, this.spinner);
-    //   }
-    // );
   }
 
   onlydigit(e) {

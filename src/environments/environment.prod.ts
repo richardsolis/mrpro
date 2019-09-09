1a2da574e5ac755ea49afdfdb87bca6a8c6eca3d
Flavio Bravo <f980534860@gmail.com>
2019-09-08 21:53:47 -0500

    agregando configuracion firebase al ambiente PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXXVfGvXrRx3YQECF1lEAGFG2wQPeT8eI",
+    authDomain: "chatmrpro.firebaseapp.com",
+    databaseURL: "https://chatmrpro.firebaseio.com",
+    projectId: "chatmrpro",
+    storageBucket: "",
+    messagingSenderId: "458934252647",
+    appId: "1:458934252647:web:742f7821ba2772f42450c2"
+  }
 };

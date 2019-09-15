6d00a1ba17c37c3c813ad8ebdf5dca4ab99d82ef
Flavio Bravo <f980534860@gmail.com>
2019-09-15 17:25:47 -0500

    integrando servicios proveedor

--- src/app/platform/provider/home/home-provider.component.ts
+++ src/app/platform/provider/home/home-provider.component.ts
@@ -5,6 +5,8 @@ import { ChatService } from '../../../services/chat.service';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { CategoryService } from 'src/app/services/category.service';
+import { DistrictService } from 'src/app/services/district.service';
 
 @Component({
   selector: 'app-home-provider',
@@ -16,23 +18,54 @@ export class HomeProviderComponent implements OnInit {
   public items: Observable<any[]>;
   public chatID: string;
 
-  constructor(db: AngularFirestore, public _cs: ChatService,
-              private _router:Router, private userService: UserService,
-              private spinner: NgxSpinnerService) {
+  public budgetsList: any[] = [];
+  districts: any[];
+  categories: any[];
+
+  constructor(db: AngularFirestore, public _cs: ChatService, private _router:Router, private userService: UserService,
+              private spinner: NgxSpinnerService,private categoryService: CategoryService, private districtService: DistrictService) {
     this.items = db.collection('chats').valueChanges();
     this.chatID = "";
+
+    this.districtService.guestGetDistricts()
+      .subscribe((response: any) => {
+        this.districts = response.data;
+      }, (error: any) => {
+        console.log(error);
+      });
+
+    this.categoryService.guestGetCategories()
+      .subscribe((response: any) => {
+        this.categories = response.data;
+      }, (error: any) => {
+        console.log(error);
+      });
+   
   }
+
   ngOnInit() {
     this.spinner.show();
     this.userService.getBudget({ type: "provider" }).subscribe(
-      response => {
+      (response:any) => {
         console.log(response);
+        this.budgetsList = response.data;
         this.spinner.hide();
       },
       error => {
         // this.generalS.relogin(this.sendBudget, error, this.spinner);
       }
     );
+  }
+
+  getName(array: any[], value){
+    let temp = "";
+    for (var i = 0; i < array.length; i++) {
+      if(array[i].id == value){
+        temp = array[i].name;
+        break;
+      }
+    }
+    return temp;
   }
 
   startChat(chatId: string){
